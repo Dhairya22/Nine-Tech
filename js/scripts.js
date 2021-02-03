49b0ce9dc0e7bd95be5aad5e82670974a41eb7f3
Dhairya22 <dhairyashelar1@gmail.com>
2021-02-03 17:17:29 +0530

    Updated Site Content

--- js/scripts.js
+++ js/scripts.js
@@ -63,12 +63,7 @@
         });
 
 
-        // SOUND TEXT TOGGLE
-        $(".sound .equalizer").on('click', function(e) {
-            $(".sound b").text(function(i, v) {
-                return v === 'Sound on' ? 'Sound off' : 'Sound on'
-            })
-        });
+        //sound
 
 
         // PARALLAX
@@ -274,29 +269,7 @@
 
 
     // EQUALIZER TOGGLE
-    var source = "audio/audio01.mp3";
-    var audio = new Audio(); // use the constructor in JavaScript, just easier that way
-    audio.addEventListener("load", function() {
-        audio.play();
-    }, true);
-    audio.src = source;
-    audio.autoplay = true;
-    audio.loop = true;
-    audio.volume = 0.2;
-
-    $('.equalizer').click();
-    var playing = true;
-    $('.equalizer').on('click', function(e) {
-        if (playing == false) {
-            audio.play();
-            playing = true;
-
-        } else {
-            audio.pause();
-            playing = false;
-        }
-    });
-
+    
 
     // EQUALIZER
     function randomBetween(range) {
